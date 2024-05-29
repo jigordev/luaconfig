81aa37ef7eff332565be2476beeaad911b0e0e06
jigordev <jigordev@gmail.com>
2024-05-29 03:07:31 -0300

    Update conversion exceptions

--- luaconfig/exceptions.py
+++ luaconfig/exceptions.py
@@ -1,14 +1,4 @@
-class LuaFuncToDict(Exception):
+class LuaFunctionConversionError(Exception):
     def __init__(self, message="Converting lua function to dict not allowed"):
-        self.message = message
-        super().__init__(self.message)
-
-class LuaFuncToJSON(Exception):
-    def __init__(self, message="Converting lua function to JSON not allowed"):
-        self.message = message
-        super().__init__(self.message)
-
-class LuaFuncToYAML(Exception):
-    def __init__(self, message="Converting lua function to YAML not allowed"):
         self.message = message
         super().__init__(self.message)
